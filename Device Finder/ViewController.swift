5079a4ff91c91c0ec1229a02e71e99211227c30d
Matthew <matthewaureliustjoa@gmail.com>
2023-10-17 21:58:01 -0400

    Initial Commit

---
+++ Device Finder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Device Finder
+//
+//  Created by Matthew Tjoa on 2023-10-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
