97bbb9f395050a95323e12c1f87b0bb62f42548c
Matthew <matthewaureliustjoa@gmail.com>
2023-10-19 17:30:09 -0400

    intial commit , all done , need to find bug and clean code

---
+++ Device Finder/MultipeerConnectionViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MultipeerConnectionViewModel.swift
+//  Device Finder
+//
+//  Created by Matthew Tjoa on 2023-10-17.
+//
+
+import Foundation
---
+++ Device Finder/NearbyInteractionViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NearbyInteractionViewModel.swift
+//  Device Finder
+//
+//  Created by Matthew Tjoa on 2023-10-17.
+//
+
+import Foundation
